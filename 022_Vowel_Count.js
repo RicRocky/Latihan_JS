81408b3d3f6314d6f5cd6768c6de99d8c48f755c
RicRocky <ryzerogtcah@gmail.com>
2024-02-04 01:56:40 +0800

    Latihan

--- 022_Vowel_Count.js
+++ 022_Vowel_Count.js
@@ -1,22 +1,22 @@
-let getCount = str => {
-    let vokal = ["a", "e", "i", "o", "u"];
-    let count = [0, 0, 0, 0, 0,]
-    let tampung = 0;
+// let getCount = str => {
+//     let vokal = ["a", "e", "i", "o", "u"];
+//     let count = [0, 0, 0, 0, 0,]
+//     let tampung = 0;
 
-    for (let i = 0; i < str.length; i++){
-        for (let j = 0; j < vokal.length; j++){
-            if (str[i] == vokal[j]){
-                count[j] += 1;
-            }
-        }
-    }
+//     for (let i = 0; i < str.length; i++){
+//         for (let j = 0; j < vokal.length; j++){
+//             if (str[i] == vokal[j]){
+//                 count[j] += 1;
+//             }
+//         }
+//     }
 
-    for (let i = 0; i < count.length; i++){
-        tampung += count[i];
-    }
+//     for (let i = 0; i < count.length; i++){
+//         tampung += count[i];
+//     }
 
-    return tampung;
-}
+//     return tampung;
+// }
 
 // let getCount = str => {
 //     let vokal = ["a", "e", "i", "o", "u"];
@@ -32,5 +32,20 @@ let getCount = str => {
 
 //     return count.reduce((a, b) => a + b, 0);
 // }
+
+let getCount = str => {
+    let vokal = ["a", "e", "i", "o", "u"];
+    let count = [0, 0, 0, 0, 0,]
+
+    for (let i = 0; i < str.length; i++){
+        for (let j = 0; j < vokal.length; j++){
+            if (str[i] == vokal[j]){
+                count[j] += 1;
+            }
+        }
+    }
+
+    return count.reduce((a, b) => a + b, 0);
+}
 
   console.log(getCount("abracadabrasdeeref"));
