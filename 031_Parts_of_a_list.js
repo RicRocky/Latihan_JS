d56a1e0bd1115420c838ba88d4e89af8fd8283a7
RicRocky <ryzerogtcah@gmail.com>
2024-02-10 03:21:37 +0800

    Latihan

--- 031_Parts_of_a_list.js
+++ 031_Parts_of_a_list.js
@@ -2,7 +2,9 @@ let partlist = arr => {
     let kalimat = [];
     let hasil = [];
     let tampung = "";
+
     for (let i = 1; i < arr.length; i++){
+        tampung = "";
         console.log(tampung);
         for (let j = 0; j < i; j++){
             if (j != (i-1)){
@@ -15,30 +17,17 @@ let partlist = arr => {
         kalimat.push(tampung);
         tampung = "";
         for (let j = i; j < arr.length; j++){
-            if (j != arr.length){
+            if (j != arr.length-1){
                 tampung += arr[j] + " ";
             }else{
                 tampung += arr[j];
             }
         }
         kalimat.push(tampung);
-        tampung = "";
         hasil.push(kalimat);
         kalimat = [];
     }
-
     return hasil;
 }
 
-/*[
-    ["I", "wish I hadn't come"], 
-    ["I wish", "I hadn't come"], 
-    ["I wish I", "hadn't come"], 
-    ["I wish I hadn't", "come"]
-]
-*/
-["I", "wish I hadn't come"], 
-["I wish", "I hadn't come"], 
-["I wish I", "hadn't come"], 
-["I wish I hadn't", "come"]
 console.log(partlist(["I", "wish", "I", "hadn't", "come"]));
