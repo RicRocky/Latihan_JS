3822f1f22f8d391d6c46039a0ab1d97797bfe341
RicRocky <ryzerogtcah@gmail.com>
2024-02-18 18:37:14 +0800

    Latihan

---
+++ 032_The_Name_Too_Long.js
@@ -0,0 +1,17 @@
+// let isRubyComing = list => {
+//     for (let i = 0; i < list.length; i++){
+//         if (list[i].language == "Ruby"){return true};
+//     }
+// }
+
+const isRubyComing = list => list.some(a => a.language == "Ruby");
+
+var list1 = [
+    { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+    { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+
+console.log(isRubyComing(list1));
+
+// Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming?]
